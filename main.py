d56ea83014c265f6e488e38d002312e0b2c5dc09
ConorQuinn1765 <conor.quinn@live.com>
2022-04-05 16:36:54 -0400

    Rearranged prompts to remove unnecessary step

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ if(len(sys.argv) == 2):
 else:
     text = input('Enter text to train the model on: ')
 
-k_gram_size = int(input('What size seed word do you want to use: '))
-writer = RandWriter(text, k_gram_size)
-
 length = int(input('How many characters do you want to generate: '))
-word = input(f'Enter a {k_gram_size} letter word to seed the new text: ')
+word = input(f'Enter a word to seed the new text(must appear in sample text): ')
+writer = RandWriter(text, len(word))
+
 print()
 
 print(writer.generate(word, length))
