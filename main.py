77ce397f608b6e8ba27efa79abdb8900db845398
ConorQuinn1765 <conor.quinn@live.com>
2022-04-05 16:24:39 -0400

    Changed wording of prompts

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ if(len(sys.argv) == 2):
 else:
     text = input('Enter text to train the model on: ')
 
-k_gram_size = int(input('Size of k_gram: '))
+k_gram_size = int(input('What size seed word do you want to use: '))
 writer = RandWriter(text, k_gram_size)
 
-length = int(input('Characters to generate: '))
-word = input(f'{k_gram_size} character word to seed text: ')
+length = int(input('How many characters do you want to generate: '))
+word = input(f'Enter a {k_gram_size} letter word to seed the new text: ')
 print()
 
 print(writer.generate(word, length))
